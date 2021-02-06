824f6f193e8f3ff4634352b05533bfd73ace9075
enesbora <boraenes84@gmail.com>
2021-02-06 14:50:21 +0100

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Zürich");
+            cities.Add("Zürich");
+            cities.Add("Zürich");
+            cities.Add("Zürich");
+            cities.Add("Zürich");
+            Console.WriteLine(cities.Count);
+            
+
+            MyList<string> cities2 = new MyList<string>();
+            cities2.Add("Luzern");
+            cities2.Add("Luzern");
+            cities2.Add("Luzern");
+            cities2.Add("Luzern");
+            cities2.Add("Luzern");
+            cities2.Add("Luzern");
+            Console.WriteLine(cities2.Count);
+        }
+    }
+
+    class MyList<T> //Generic class
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
